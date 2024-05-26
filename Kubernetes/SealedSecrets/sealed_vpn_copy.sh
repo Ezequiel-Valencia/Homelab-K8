3b37c046fad6c042250e512f3585badd589b16ab
Zek <zek@pop-os.localdomain>
2024-05-26 13:14:15 -0400

    New Secrets
    Seems as if the old credentials where wrong

--- Kubernetes/SealedSecrets/sealed_vpn_copy.sh
+++ Kubernetes/SealedSecrets/sealed_vpn_copy.sh
@@ -3,7 +3,7 @@
 # Example: ./sealed_vpn.sh wireguard_key address mullvad > output.yaml
 
 # validate the number of arguments
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters"
     echo "Usage: ./sealed_secret_api.sh <wireguard_private_key> <wireguard_address> <vpn_service_provider>"
     exit 1
@@ -11,20 +11,14 @@ fi
 
 SECRET_NAME="vpn-secrets"
 NAMESPACE="media"
-WIREGUARD_PRIVATE_KEY=$1
-WIREGUARD_ADDRESSES=$2
-VPN_SERVICE_PROVIDER=$3
+OPENVPN_USER=$1
+VPN_PROVIDER=$2
 
 # Broken on purpose since it has production configurations, remove kubconfig flag to bring it back to test
 
 kubectl create secret generic ${SECRET_NAME} --dry-run=client --kubeconfig=/home/zek/.kube/config_prd \
-      --from-literal=WIREGUARD_PRIVATE_KEY="${WIREGUARD_PRIVATE_KEY}" \
-      --from-literal=SERVER_CITIES="New York NY" \
-      --from-literal=WIREGUARD_ADDRESSES="${WIREGUARD_ADDRESSES}" \
-      --from-literal=VPN_SERVICE_PROVIDER="${VPN_SERVICE_PROVIDER}" \
-      --from-literal=VPN_TYPE="wireguard" \
-      --from-literal=DOT_PROVIDERS="quad9" \
-      --from-literal=UPDATER_PERIOD="48h" \
+      --from-literal=OPENVPN_USER="${OPENVPN_USER}" \
+      --from-literal=VPN_SERVICE_PROVIDER="${VPN_PROVIDER}" \
       --namespace="${NAMESPACE}" -o yaml | \
       kubeseal --kubeconfig=/home/zek/.kube/config_prd \
       --format yaml
