5fbd5eee8b12423dd1ae9db5cab9ed3cd6c710d7
Zek <zek@pop-os.localdomain>
2024-05-26 15:27:30 -0400

    Certs for now

--- Kubernetes/helm/install-charts.sh
+++ Kubernetes/helm/install-charts.sh
@@ -7,6 +7,12 @@ helm repo add traefik https://traefik.github.io/charts
 
 helm install traefik traefik/traefik --version 28.1.0 --namespace traefik-ingress --values=/home/zek/Documents/Code/Homelab/Kubernetes/helm/minikube/traefik-values.yml
 
+helm install cert-manager jetstack/cert-manager \
+ --version v1.14.5 --create-namespace \
+ --namespace cert-manager  \
+ --values=/home/zek/Documents/Code/Homelab/Kubernetes/helm/minikube/certmanager-values.yml \
+ --kubeconfig=/home/zek/.kube/config_prd
+
 helm uninstall --namespace=traefik-ingress traefik
 
 helm list --namespace=traefik-ingress --output yaml
