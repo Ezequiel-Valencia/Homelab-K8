ef752397c1e32ab26d9f5e7e2b6d918ba4313b2b
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-09-04 19:26:30 -0400

    Added Semaphore

---
+++ Kubernetes/SealedSecrets/production/semaphore.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Example: ./sealed_vpn.sh wireguard_key address mullvad > output.yaml
+
+SECRET_NAME="semaphore-secrets"
+NAMESPACE="monitoring"
+read -s -p "DB Password: " SEMAPHORE_DB_PASS
+echo
+read -s -p "Semaphore Access Key: " SEMAPHORE_ACCESS_KEY_ENCRYPTION
+echo
+read -s -p "Semaphore Admin Password: " SEMAPHORE_ADMIN_PASSWORD
+
+
+# Broken on purpose since it has production configurations, remove kubconfig flag to bring it back to test
+
+kubectl create secret generic ${SECRET_NAME} --dry-run=client --kubeconfig=/home/zek/.kube/config_prd \
+      --from-literal=SEMAPHORE_DB_PASS="${SEMAPHORE_DB_PASS}" \
+      --from-literal=SEMAPHORE_ACCESS_KEY_ENCRYPTION="${SEMAPHORE_ACCESS_KEY_ENCRYPTION}" \
+      --from-literal=SEMAPHORE_ADMIN_PASSWORD="${SEMAPHORE_ADMIN_PASSWORD}" \
+      --from-literal=POSTGRES_PASSWORD="${SEMAPHORE_DB_PASS}" \
+      --namespace="${NAMESPACE}" -o yaml | \
+      kubeseal --kubeconfig=/home/zek/.kube/config_prd \
+      --format yaml > ./semaphore.yml
