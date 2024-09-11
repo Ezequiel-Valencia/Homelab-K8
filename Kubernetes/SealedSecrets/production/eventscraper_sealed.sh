e39ce93fc9d9e56b18f5673392a1d8537cb68695
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-09-11 18:56:44 -0400

    Event Scraper as Deployment with Slack Messages

--- Kubernetes/SealedSecrets/production/eventscraper_sealed.sh
+++ Kubernetes/SealedSecrets/production/eventscraper_sealed.sh
@@ -7,11 +7,14 @@ NAMESPACE="bots"
 read -s -p "Mobilizon Email: " MOBILIZON_EMAIL
 echo
 read -s -p "Mobilizon Password: " MOBILIZON_PASSWORD
+echo
+read -s -p "Slack Webhook: " SLACK_WEBHOOK
 
 
 kubectl create secret generic ${SECRET_NAME} --dry-run=client --kubeconfig=/home/zek/.kube/config_prd \
       --from-literal=MOBILIZON_EMAIL="${MOBILIZON_EMAIL}" \
       --from-literal=MOBILIZON_PASSWORD="${MOBILIZON_PASSWORD}" \
+      --from-literal=SLACK_WEBHOOK="${SLACK_WEBHOOK}" \
       --namespace="${NAMESPACE}" -o yaml | \
       kubeseal --kubeconfig=/home/zek/.kube/config_prd \
       --format yaml > ./eventscraper-secrets.yml
